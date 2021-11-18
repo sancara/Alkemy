0b28847989495b757ac052589629cdcbb4a7e251
Sancara <santiagocaraballo@gmail.com>
2021-11-18 20:10:40 -0300

    primer desafío Pre-Alkemy

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+/*
+1.  Definir una tupla que describa una dirección, con campos como ciudad, partido, provincia, calle, pais, codigoPostal, etc. Siéntanse libres de colocar todos los campos que consideren relevantes. Usar un diccionario para la calle con los campos nombreDeCalle, numero, entrecalle1 y entrecalle2.
+ */
+let tupla = (ciudad:"Paraná", partido: "Paraná" ,provincia:"Entre Ríos" , calle:["nombreCalle": "Concordia", "numero":"486", "entrecalle1":"Gualeguaychú", "entrecalle2":"Rioja"] , pais:"Argentina" ,codigoPostal: "3100")
+
+
+/*
+2.   Dentro de la dirección, definir algunos tipos  de datos opcionales, entre ellos piso y        departamento.
+ 
+*/
+
+
+
+/*
+3.   Definir tres direcciones en constantes.
+4.   Escribir una función que reciba una            dirección y la imprima como un String bien     formateado. Hacer uso de la interpolación.
+5.   Escribir una función que reciba un Array de    direcciones y devuelva un String que contenga  "piso: \(piso) ; depto: \(departamento)",      SOLO para las direcciones que tengan           definidos tanto un piso como un departamento.
+*/

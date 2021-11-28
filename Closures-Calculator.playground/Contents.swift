2165096c624791df4298f4da6816cf6950b9e4ea
Sancara <santiagocaraballo@gmail.com>
2021-11-28 20:29:17 -0300

    Adding a Filter func to calc

--- Closures-Calculator.playground/Contents.swift
+++ Closures-Calculator.playground/Contents.swift
@@ -6,12 +6,26 @@ struct Calculator {
     }
     return total
   }
+
+  func filter(_ nums: [Int], using isIncluded: (Int) -> Bool) -> [Int] {
+      var filteredNums = [Int]()
+      for num in nums {
+        if isIncluded(num){
+          filteredNums.append(num)
+        }
+      }
+    return filteredNums
+  }
 }
 
 let calc = Calculator()
-let numbers = [1,2,3,5,6,7,8]
+let numbers = [1,2,3]
 
 let addition: (Int, Int) -> Int = { $0 + $1 }
 
 let sum = calc.reduce(numbers, using: addition, startingAt: 0)
 print(sum)
+
+let rainfallInInches = [5,2,8,0,0,4]
+let rainyDays = calc.filter(rainfallInInches, using: { rainFall in rainFall != 0 })
+print(rainyDays)

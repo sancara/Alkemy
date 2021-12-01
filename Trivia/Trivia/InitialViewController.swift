5a9a9917a58ffefd3fbb1c33f3f6db679df1e928
Sancara <santiagocaraballo@gmail.com>
2021-12-01 12:17:24 -0300

    alkemy trivia

---
+++ Trivia/Trivia/InitialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InitialViewController.swift
+//  Trivia
+//
+//  Created by Santiago Caraballo on 01/12/2021.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    @IBOutlet weak var alkemyLogoStart: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        alkemyLogoStart.layer.cornerRadius = 70
+        alkemyLogoStart.layer.borderColor = UIColor.white.cgColor
+        alkemyLogoStart.layer.borderWidth = 4
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Trivia/Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Santiago Caraballo on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
